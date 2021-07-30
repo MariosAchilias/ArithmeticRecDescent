439ea09ebfab97a6a661648c6cd8e42367a4e3f1
Marios Achilias <marios.axilias@protonmail.com>
2021-07-30 20:18:04 +0300

    Minor changes

--- README.md
+++ README.md
@@ -2,18 +2,28 @@
 
 Simple program for arithmetic expression evaluation using recursive descent parsing.
 
+## Usage
+
+    cargo build
+    cd target/debug/
+    ./arithmetic_rec_descent
+    1+2+3+4
+
+
 ## LL(1) Grammar
 
-expr -> term expr_cont
-expr_cont -> + term expr_cont
-           | - term expr_cont
-           | ε
-term -> factor term_cont
-term_cont -> * factor term_cont
-           | / factor term_cont
-           | ε
-factor -> (expr)
-        | num
-		| - num
-num -> digit num_cont
-num_cont -> digit num_cont | ε
+    expr -> term expr_cont.
+    expr_cont -> + term expr_cont
+               | - term expr_cont
+               | ε.
+    term -> factor term_cont
+    term_cont -> * factor term_cont
+               | / factor term_cont
+               | ε.
+    factor -> (expr)
+            | num
+    		| -factor2.
+    factor2 -> (expr)
+    		| num.
+    num -> digit num_cont.
+    num_cont -> digit num_cont | ε.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 fn main() {
 
 	// Read expression
-	let mut s: String = String::new();
-	std::io::stdin().read_line(&mut s);
+	let mut expression: String = String::new();
+    std::io::stdin().read_line(&mut expression);
 
 	// Remove whitespace
-	s.retain(|c| !c.is_whitespace());	
+	expression.retain(|c| !c.is_whitespace());	
 
 	// Split into tokens and store in vector
-    let tokens: Vec<String> = tokenize(s);
+    let tokens: Vec<String> = tokenize(expression);
 
     // Position of lookahead token in vector
     let mut lookahead: usize = 0;
 
     let result = expr(&mut lookahead, &tokens);
 
-    println!("{:?}", tokens);
-    println!("Result: {}", result);
+    println!("Result: {}\n", result);
 }
 
 fn tokenize(s: String) -> Vec<String>{
@@ -37,7 +36,7 @@ fn tokenize(s: String) -> Vec<String>{
 
     // Not all characters copied
     if copied < s.len() {
-        // Append to vector the text from last delimiter to end of string
+        // Append to vector the remaining text from last delimiter to end of string
         result.push(s[copied..].to_string());
     }
 
@@ -46,49 +45,63 @@ fn tokenize(s: String) -> Vec<String>{
 
 // Returns true if lookahead type matches token type given as argument
 fn matches(tok_type: &str, lookahead: usize, tokens: &Vec<String>) -> bool{
+    // If token doesn't exist, return false
+    if lookahead >= tokens.len(){return false;}
     match tok_type{
         // Lookahead is numeric
         "num" => tokens[lookahead].parse::<i32>().is_ok(),
-        // Valid operator
-        "op" => {tokens[lookahead] == "+" || tokens[lookahead] == "-"},
-        // Valid factor (either num or parenthesis)
-        "factor" => tokens[lookahead].parse::<i32>().is_ok() || tokens[lookahead] == "(" || tokens[lookahead] == ")" || tokens[lookahead] == "-",
-        // Empty string ε
-        "empty" => lookahead == tokens.len(),
-        // Left parenthesis
+        // Valid factor (num, parenthesis, or -factor2)
+        "factor" =>{
+            tokens[lookahead].parse::<i32>().is_ok()
+            || tokens[lookahead] == "("
+            || tokens[lookahead] == ")"
+            || (tokens[lookahead] == "-" && matches("factor2", lookahead+1, tokens))
+        },
+        "factor2" =>{
+            tokens[lookahead].parse::<i32>().is_ok()
+            || tokens[lookahead] == "("
+            || tokens[lookahead] == ")"
+        },
         "left_par" => tokens[lookahead] == "(",
-        // Right parenthesis
         "right_par" => tokens[lookahead] == ")",
+        "minus" => tokens[lookahead] == "-",
+        "plus" => tokens[lookahead] == "+",
+        "mult" => tokens[lookahead] == "*",
+        "div" => tokens[lookahead] == "/",
         _ => false
     }
 
 }
 
 fn expr(lookahead:&mut usize, tokens: &Vec<String>) -> i32{
-    println!("In expr lookahead:{} \n", *lookahead);
     let term_value:i32 = term(lookahead, tokens);
     return expr_cont(lookahead, tokens, term_value);
 }
 
 fn expr_cont(lookahead:&mut usize, tokens: &Vec<String>, value_so_far: i32) -> i32{
-    println!("In expr_cont lookahead: {}\n", *lookahead);
+
+    // Last component of string is expr_cont,
+    // check if end of original expression reached
     if *lookahead == tokens.len(){
         return value_so_far;
     }
     
-    if tokens[*lookahead] == "+"{
+    // + term expr_cont
+    if matches("plus", *lookahead, tokens){
         *lookahead = *lookahead + 1;
         let addition_result: i32 = value_so_far + term(lookahead, tokens);
         return expr_cont(lookahead, tokens, addition_result);
     }
 
-    if tokens[*lookahead] == "-"{
+    // - term expr_cont
+    if matches("minus", *lookahead, tokens){
         *lookahead = *lookahead + 1;
         let sub_result: i32 = value_so_far - term(lookahead, tokens);
         return expr_cont(lookahead, tokens, sub_result);
     }
 
-    value_so_far
+    // ε
+    return value_so_far;
 
 }
 
@@ -98,50 +111,80 @@ fn term(lookahead:&mut usize, tokens: &Vec<String>) -> i32{
         *lookahead = *lookahead + 1;
         return term_cont(lookahead, tokens, factor_value);
     }
-    panic!("Error! Improperly formatted expression");
+    panic!("Error! Invalid expression\n");
 }
 
 fn term_cont(lookahead:&mut usize, tokens: &Vec<String>, value_so_far: i32) -> i32{
-    
-    // End of string reached, check before using *lookahead as index
-    if matches("empty", *lookahead, tokens){
+
+    // Last component of string is expr_cont, last component of expr_cont is term_cont,
+    // check here if end of original expression string reached, before using *lookahead as index
+    if *lookahead == tokens.len(){
         return value_so_far;
     }
 
-    if tokens[*lookahead] == "*"{
+    // * factor term_cont
+    if matches("mult", *lookahead, tokens){
         *lookahead = *lookahead + 1;
         let mult_val: i32 = value_so_far * factor(lookahead, tokens);
         *lookahead = *lookahead + 1;
         return term_cont(lookahead, tokens, mult_val);
     }
     
-    if tokens[*lookahead] == "/"{
+    // / factor term_cont
+    if matches("div", *lookahead, tokens){
         *lookahead = *lookahead + 1;
-        let fact_val: i32 = factor(lookahead, tokens);
+        let divisor: i32 = factor(lookahead, tokens);
         *lookahead = *lookahead + 1;
-        return value_so_far / fact_val;
+        if divisor == 0{
+            panic!("Error! Division by zero\n");
+        }
+        return value_so_far / divisor;
     }
 
-    // ε case
-    value_so_far
+    // ε
+    return value_so_far;
 }
 
 fn factor(lookahead:&mut usize, tokens: &Vec<String>) -> i32{
+    // (expr)
     if matches("left_par", *lookahead, tokens){
         *lookahead = *lookahead + 1;
         let parenthesis_value: i32 = expr(lookahead, tokens);
         if !matches("right_par", *lookahead, tokens){
-            println!("Error! Unclosed parenthesis\n");
-            return 0;
+            panic!("Error! Invalid expression\n");
         }
         return parenthesis_value;
     }
-    if matches("op", *lookahead, tokens){
-        if(tokens[*lookahead] == "-"){
-            *lookahead = *lookahead+1;
-            return tokens[*lookahead].parse::<i32>().unwrap() * -1;
-        }
-        *lookahead = *lookahead + 1;
+    // num
+
+    if matches("num", *lookahead, tokens){
+        return tokens[*lookahead].parse().unwrap();
     }
-    return tokens[*lookahead].parse().unwrap();
+
+    // -factor2
+    if matches("minus", *lookahead, tokens){
+        *lookahead = *lookahead+1;
+        return factor2(lookahead, tokens) * -1;
+    }
+
+    panic!("Error! Invalid expression\n");
+}
+
+fn factor2(lookahead:&mut usize, tokens: &Vec<String>) -> i32{
+    // (expr)
+    if matches("left_par", *lookahead, tokens){
+        *lookahead = *lookahead + 1;
+        let parenthesis_value: i32 = expr(lookahead, tokens);
+        if !matches("right_par", *lookahead, tokens){
+            panic!("Error! Invalid expression(unclosed parentheses)\n");
+        }
+        return parenthesis_value;
+    }
+    // num
+    if matches("num", *lookahead, tokens){
+        return tokens[*lookahead].parse().unwrap();
+    }
+
+    panic!("Error! Invalid expression\n");
+
 }
